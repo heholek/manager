5e8840d95bc3fc8ad75775c783a27d5704072c83
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-02-06 16:55:57 +0100

    feat(hub): add chatbot integration
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/hub/src/index.js
+++ packages/manager/apps/hub/src/index.js
@@ -1,5 +1,6 @@
 import 'script-loader!jquery'; // eslint-disable-line
 import 'script-loader!moment/min/moment-with-locales.min.js'; //eslint-disable-line
+
 import { Environment } from '@ovh-ux/manager-config';
 import angular from 'angular';
 import 'angular-translate';
@@ -18,6 +19,7 @@ import atInternet from './components/at-internet';
 import preload from './components/manager-preload';
 import dashboard from './dashboard';
 
+import controller from './controller';
 import routing from './routing';
 import './index.scss';
 import 'ovh-ui-kit/dist/oui.css';
@@ -39,8 +41,10 @@ angular
       ovhManagerOrderTracking,
       preload,
       uiRouter,
+      __NG_APP_INJECTIONS__,
     ].filter(isString),
   )
+  .controller('HubController', controller)
   .config(
     /* @ngInject */ ($locationProvider) => $locationProvider.hashPrefix(''),
   )
@@ -50,7 +54,12 @@ angular
       $transitions.onBefore({ to: 'app.**' }, () => $translate.refresh());
     },
   )
-  .run(($translate) => {
-    moment.locale(head($translate.use().split('_')));
-  })
+  .config(
+    /* @ngInject */ (TranslateServiceProvider) => {
+      const defaultLanguage = TranslateServiceProvider.getUserLocale();
+      // set moment locale
+      moment.locale(head(defaultLanguage.split('_')));
+    },
+  )
   .run(/* @ngTranslationsInject:json ./translations */);
+
