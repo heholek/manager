0a2ef0730a8b145f578e92e2fcd2abe9bd693697
Marie JONES <marie.jones@corp.ovh.com>
2020-03-03 10:08:46 +0100

    feat(dashboard): add ovh-manager-banner
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/index.js
+++ packages/manager/apps/hub/src/dashboard/index.js
@@ -2,6 +2,7 @@ import 'script-loader!jquery'; // eslint-disable-line
 import angular from 'angular';
 import 'angular-translate';
 
+import ovhManagerBanner from '@ovh-ux/manager-banner';
 import ovhManagerHub from '@ovh-ux/manager-hub';
 
 import component from './dashboard.component';
@@ -9,7 +10,11 @@ import component from './dashboard.component';
 const moduleName = 'ovhManagerHubDashboard';
 
 angular
-  .module(moduleName, ['pascalprecht.translate', ovhManagerHub])
+  .module(moduleName, [
+    'pascalprecht.translate',
+    ovhManagerBanner,
+    ovhManagerHub,
+  ])
   .component('hubDashboard', component)
   .run(/* @ngTranslationsInject:json ./translations */);
 
